9d26efa4091e11a0572fb08d0c4066ff8879f1b0
Ahkar Hein <ahkarhein04@gmail.com>
2026-07-24 19:39:51 -0700

    add bubble sort algorithms with double loop an temp swap

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center text-white p-8">
+        AlgoViz 🧠
+      </h1>
+    </div>
+  )
+}
+
+export default App
